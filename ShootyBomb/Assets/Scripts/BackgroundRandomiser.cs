768fa5d76f72b1978d1a1672ac1a67d5bd369791
DESKTOP-WindowsPC <nathan.nette@gmail.com>
2018-08-15 20:51:17 -0700

    Gun shoots bullets with physics how I want it. Colliders around edges of screen. Considering a game where you need to keep a balloon in the air by shooting it.

---
+++ ShootyBomb/Assets/Scripts/BackgroundRandomiser.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundRandomiser : MonoBehaviour {
+
+	// Array of background images.
+	public Sprite[] m_Background;
+
+	// To cache the sprite renderer.
+	private SpriteRenderer m_SpriteRenderer;
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_SpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+		m_SpriteRenderer.sprite = m_Background[RandomNum()];
+		m_SpriteRenderer.flipX = RandomBool();
+	}
+
+	// Spits out a random int between 0 and the size of the background array.
+	private int RandomNum()
+	{
+		int rand = Random.Range(0, m_Background.Length);
+		return rand;
+	}
+
+	// Spits out a random boolean value.
+	private bool RandomBool()
+	{
+		int rand = Random.Range(0,2);
+
+		if(rand == 0)
+			return false;
+		else
+			return true;
+	}
+}
---
+++ ShootyBomb/Assets/Scripts/GunControl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunControl : MonoBehaviour {
+
+	public GameObject m_Bullet;
+	public GameObject m_ShootPoint;
+	public Camera m_MainCam;
+	public float m_ShootPower;
+	private Vector3 m_MousePos;
+	private Vector3 m_ObjectPos;
+	private float m_GunAngle;
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		CalcRotation();
+		if(Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			Shoot();
+		}
+	}
+
+	// Was struggling to get the rotation of the gun right.
+	// https://answers.unity.com/questions/10615/rotate-objectweapon-towards-mouse-cursor-2d.html
+	// This forum post helped me out.
+	private void CalcRotation()
+	{
+		
+         m_MousePos = Input.mousePosition;
+         m_MousePos.z = 5.23f;
+ 
+         m_ObjectPos = m_MainCam.WorldToScreenPoint (transform.position);
+         m_MousePos.x = m_MousePos.x - m_ObjectPos.x;
+         m_MousePos.y = m_MousePos.y - m_ObjectPos.y;
+ 
+         m_GunAngle = Mathf.Atan2(m_MousePos.y, m_MousePos.x) * Mathf.Rad2Deg;
+         transform.rotation = Quaternion.Euler(new Vector3(0, 0, m_GunAngle));
+	}
+
+	private void Shoot()
+	{
+		GameObject bullet = GameObject.Instantiate(m_Bullet, m_ShootPoint.transform.position, Quaternion.Euler(new Vector3(0, 0, m_GunAngle)));
+
+		bullet.GetComponent<Rigidbody2D>().AddForce((new Vector2(m_MousePos.x, m_MousePos.y).normalized * m_ShootPower), ForceMode2D.Impulse);
+
+	}
+}
