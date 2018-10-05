1aa361c0b68ea2ff26d94714893641570a53778d
Nathan Nette <nathan.nette@gmail.com>
2018-10-05 15:14:16 +1000

    Added photon but still laggy.

---
+++ ShootyBomb/Assets/Scripts/OnPlayerNetworkLoad.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnPlayerNetworkLoad : MonoBehaviour {
+
+	private GameObject m_Player = null;
+
+	public string m_PrefabName;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (!m_Player)
+            {
+                if (m_Player = GameObject.Find(m_PrefabName + "(Clone)"))
+                {
+					Debug.Log("Hit");
+                    m_Player.GetComponent<PlayerCtrler>().enabled = true;
+                }
+            }
+	}
+}
--- ShootyBomb/Assets/Scripts/PlayerCtrler.cs
+++ ShootyBomb/Assets/Scripts/PlayerCtrler.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerCtrler : MonoBehaviour
+{
 
 public float m_RunSpeed;
 public float m_MaxSpeed;
+public float m_Downforce;
+public float m_JumpForce;
 private Animator m_PlayerAnimator;
 private SpriteRenderer m_PlayerSprRend;
 private Rigidbody2D m_RB2D;
@@ -21,17 +24,17 @@ private Rigidbody2D m_RB2D;
 	// Update is called once per frame
 	void Update () 
 	{
-		Debug.Log(m_RB2D.velocity.magnitude);
+		//Debug.Log(m_RB2D.velocity.magnitude);
 		InputCheck();
-
-
+		PushDown();
+		
 	}
 
 	private void InputCheck()
 	{
 		if(Input.GetKey(KeyCode.D))
 		{
-			m_RB2D.AddForce((Vector2.right.normalized * m_RunSpeed), ForceMode2D.Force);
+			m_RB2D.AddForce((gameObject.transform.right.normalized * m_RunSpeed), ForceMode2D.Force);
 			if(m_RB2D.velocity.magnitude > m_MaxSpeed)
 			{
 				float diff = m_RB2D.velocity.magnitude - m_MaxSpeed;
@@ -42,7 +45,7 @@ private Rigidbody2D m_RB2D;
 		}
 		else if(Input.GetKey(KeyCode.A))
 		{
-			m_RB2D.AddForce((Vector2.left.normalized * m_RunSpeed), ForceMode2D.Force);
+			m_RB2D.AddForce((-gameObject.transform.right.normalized * m_RunSpeed), ForceMode2D.Force);
 			if(m_RB2D.velocity.magnitude > m_MaxSpeed)
 			{
 				float diff = m_RB2D.velocity.magnitude - m_MaxSpeed;
@@ -55,5 +58,21 @@ private Rigidbody2D m_RB2D;
 		{
 			m_PlayerAnimator.SetBool("isRunning", false);
 		}
+
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			m_RB2D.AddForce(gameObject.transform.up.normalized * m_JumpForce, ForceMode2D.Impulse);
+		}
+
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
 	}
+	private void PushDown()
+	{
+		m_RB2D.AddForce((new Vector2(gameObject.transform.up.x * -1, gameObject.transform.up.y * -1).normalized * m_Downforce));
+	}
+
+	
 }
